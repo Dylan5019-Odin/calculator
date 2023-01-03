dd56822b54656754db3881ba5874185035e1de31
Dylan5019-Odin <84342823+Dylan5019-Odin@users.noreply.github.com>
2023-01-03 14:41:22 -0600

    Add Operate Function

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
-
 //Claulcator Functions
 
-const add = (x,y) => {return (x+y)};
+const add = (x, y) => {
+  return x + y;
+};
 
-const subtract = (x,y) => {return (x-y)};
+const subtract = (x, y) => {
+  return x - y;
+};
 
-const multiply = (x,y) => {return (x*y)};
+const multiply = (x, y) => {
+  return x * y;
+};
 
-const divide = (x,y) => {return (x/y)};
+const divide = (x, y) => {
+  return x / y;
+};
+
+const operate = (operator, x, y) => {
+  return operator == "/"
+    ? divide(x, y)
+    : operator == "*"
+    ? multiply(x, y)
+    : operator == "+"
+    ? add(x, y)
+    : operator == "-"
+    ? subtract(x, y)
+    : 0;
+};
